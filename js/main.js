835af655a72537e7cd99c7e0b77b0a6ae7666486
dilobar00 <dilobarmahmudjonova@gmail.com>
2024-05-26 00:09:32 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+var task1 = prompt("Teglar nechi turga bolinadi va ular qaysilar?")
+var task2 = prompt ("Listlar oldida paydo boladigan nuqtalarni qaysi kod orqali yoq qilish mumkin?")
+var task3 = prompt("Qaysi soz element ichida soya hosil qilishda yordam beradi?")
+var task4 = prompt("transform-origin uchun boshlangich, ozgarmagan holatni kiritng")
+var task5 = prompt ("CSS da fontni qalin qilib yozish uchun qaysi qiymatdan foydalaniladi ?")
+var task6 = prompt(":link, :visited, :active va :hover tanlovchilarining umumiy nomi nima?")
+var task7 = prompt(" hue-rotate(120deg) kodni kiritish orqali qanday rangga ega bolamiz?")
+var task8 = prompt("HTML nechi bolimdan iborat va ular qaysilar?")
+var task9 = prompt("HTML faylni CSS ga ulash usullari?")
+var task10 = prompt("<br> tagini vaziafsi")
+var task11 = prompt("block va inline elementlar farqi?")
+var task12 = prompt("width=100 va width=100% bir xil manoga egami?")
+var task13 = prompt("Web sahifa url manzil berish uchun qanday tegdan foydalanamiz?")
+var task14 = prompt("Qaysi atribut orqali biz url manzilini linkka joylaymiz?")
+var task15 = prompt("uzun line (chiziq) chizish uchun qanday  teg dan foydalanamiz?")
+
+var result = {
+    task1 : task1,
+    task2 : task2,
+    task3 : task3,
+    task4 : task4,
+    task5 : task5,
+    task6 : task6,
+    task7 : task7,
+    task8 : task8,
+    task9 : task9,
+    task10 : task10,
+    task11 : task11,
+    task12 : task12,
+    task13 : task13,
+    task14 : task14,
+    task15 : task15,
+   
+}
+console.log(result);
